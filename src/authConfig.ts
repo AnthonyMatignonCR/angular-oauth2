905cfae02a27493da04b8cb23ca1eb0905f63e52
Anthony Matignon <anthony.matignon@codes-rousseau.fr>
2021-08-17 11:47:03 +0200

    Change oauth config

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,15 +1,14 @@
 import {AuthConfig} from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'https://localhost',
+  issuer: 'https://easysysteme.fr',
   redirectUri: window.location.origin + '/',
-  loginUrl: 'https://localhost/vues/authorize',
-  clientId: '4b0041110efa883e1555f4a6024f7da1',
-  dummyClientSecret: 'a588dd51435afa7a9d2b61a51f008ecd349eff44d36de246bff9400efb7193bc6b4cf6a780b0111e52ba76a4af57ab4eb4508c830defed50a4d9c20087ea03d9',
+  loginUrl: 'https://easysysteme.fr/vues/authorize',
+  clientId: 'ef204316df44f127ede1d3ea9714c0ef',
   responseType: 'code',
   scope: 'openid user',
   showDebugInformation: true,
   requireHttps: false,
   oidc: true,
-  tokenEndpoint: 'https://localhost/api/v2/token',
+  tokenEndpoint: 'https://easysysteme.fr/api/v2/token',
 };
